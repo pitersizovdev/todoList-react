aaeef956b16038ea88dcffaad38244525fb1ed4b
pitersizovdev <pitersizov@gmail.com>
2024-02-04 18:12:47 +0300

    0.0.7

--- src/components/add/add.jsx
+++ src/components/add/add.jsx
@@ -2,9 +2,29 @@ import React, {Component} from 'react'
 import './add.css'
 
 export default class Add extends Component{
+
+    state ={
+        label:''
+    }
+
+    lableChange =(e)=>{
+        this.setState({
+            label: e.target.value
+        })
+    }
+    onSubmit =(e)=>{
+        e.preventDefault()
+        this.props.add(this.state.label)
+        this.setState({
+            label:''
+        })
+    }
+
   render(){
-    return(<div className='item-add-form'>
-        <button className='btn btn-outline-secondary item-add-form' onClick={()=>this.props.add('txtbnj')}>Add Item</button>
-    </div>
+    return(
+    <form className='item-add-form' onSubmit={this.onSubmit}>
+        <input type='text' className='form-control' onChange={this.lableChange} placeholder='Write a task' value={this.state.label}></input>
+        <button className='btn btn-outline-secondary item-add-form' >Add Item</button>
+    </form>
   )} 
 }
